f064883f34763da2c4a11244a2a757a95d894f71
Ruby Dennington <theleruby@gmail.com>
2026-08-07 18:35:20 +0100

    Print blank lines above and below fastfetch output

--- build_files/misc/theledora-neofetch.sh
+++ build_files/misc/theledora-neofetch.sh
@@ -1,3 +1,3 @@
-alias neofetch='/usr/bin/fastfetch --color "0;38;2;240;80;180" -c /usr/share/ublue-os/theledora/fastfetch.jsonc'
-alias neowofetch='/usr/bin/fastfetch --color "0;38;2;240;80;180" -c /usr/share/ublue-os/theledora/fastfetch.jsonc'
-alias fastfetch='/usr/bin/fastfetch --color "0;38;2;240;80;180" -c /usr/share/ublue-os/theledora/fastfetch.jsonc'
+alias neofetch='echo "" && /usr/bin/fastfetch --color "0;38;2;240;80;180" -c /usr/share/ublue-os/theledora/fastfetch.jsonc && echo ""'
+alias neowofetch='echo "" && /usr/bin/fastfetch --color "0;38;2;240;80;180" -c /usr/share/ublue-os/theledora/fastfetch.jsonc && echo ""'
+alias fastfetch='echo "" && /usr/bin/fastfetch --color "0;38;2;240;80;180" -c /usr/share/ublue-os/theledora/fastfetch.jsonc && echo ""'
